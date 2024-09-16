51400a00b897f919e04c6c569bee7362878e05b4
Rahul Kumar Sah <rahulrks9805@gmail.com>
2024-09-16 13:11:18 +0530

    js

---
+++ 1basics.js
@@ -0,0 +1,25 @@
+console.log(num);
+solve();
+
+var num = 14;
+
+function solve(){
+    console.log(num);
+}
+
+// --------------------------------
+
+console.log(window);
+console.log(this);
+console.log(this === window);
+
+
+var a = 20;
+console.log(a);
+console.log(window.a);
+console.log(this.a);
+
+let b = 100;
+console.log(b);
+console.log(window.b);
+console.log(this.b);
--- callbackfunc.js
+++ callbackfunc.js
@@ -1,6 +1,6 @@
-// function add(num1,num2){
-//     return num1+num2;
-// }
+function add(num1,num2){
+    return num1+num2;
+}
 
 
 // function isEven(func){
@@ -17,9 +17,17 @@
 
 
 
-// setTimeout(function(){
-//     console.log(5+6);
-// },2000);
+setTimeout(function(){
+    console.log(5+6);
+},2000);
+
+
+
+setTimeout((name="Rahul",age=23)=>{
+    console.log(`Hi, my name is ${name} and my age is ${age}`);
+},4000);
+
+
 
 
 // let count = 0;
@@ -31,39 +39,39 @@
 
 
 
-function solve(){
-    document.getElementById('submit').addEventListener("click", function print(){
-        for(let i = 1; i<=5; i++){
-            setTimeout(function(){
-                console.log(i);
-            }, i*1000);
-        }
-        setTimeout(() => {
-            console.log("End");
-        }, 6000);
-    })
-}
+// function solve(){
+//     document.getElementById('submit').addEventListener("click", function print(){
+//         for(let i = 1; i<=5; i++){
+//             setTimeout(function(){
+//                 console.log(i);
+//             }, i*1000);
+//         }
+//         setTimeout(() => {
+//             console.log("End");
+//         }, 6000);
+//     })
+// }
 
-solve();
+// solve();
 
 
 
-function program(){
-    document.getElementById('one').addEventListener('click', function(){
-        console.log(1);
-    })
-    document.getElementById('two').addEventListener('click', function(){
-        console.log(2);
-    })
-    document.getElementById('three').addEventListener('click', function(){
-        console.log(3);
-    })
-    document.getElementById('four').addEventListener('click', function(){
-        console.log(4);
-    })
-    document.getElementById('equal').addEventListener('click', function(){
-        console.log("Equal");
-    })
-}
+// function program(){
+//     document.getElementById('one').addEventListener('click', function(){
+//         console.log(1);
+//     })
+//     document.getElementById('two').addEventListener('click', function(){
+//         console.log(2);
+//     })
+//     document.getElementById('three').addEventListener('click', function(){
+//         console.log(3);
+//     })
+//     document.getElementById('four').addEventListener('click', function(){
+//         console.log(4);
+//     })
+//     document.getElementById('equal').addEventListener('click', function(){
+//         console.log("Equal");
+//     })
+// }
 
-program();
+// program();
